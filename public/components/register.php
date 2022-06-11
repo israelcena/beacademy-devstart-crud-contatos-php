ad5be2d4336e540ce86d4072d0431b8c66add482
israelcena <israelsjm@gmail.com>
2022-06-11 17:02:13 -0300

    include data adpter

--- public/components/register.php
+++ public/components/register.php
@@ -2,19 +2,19 @@
   <div class="container col-xl-3 mt-5">
     <!-- Name input -->
     <div class="form-outline mb-4">
-      <input type="text" id="name" name="name" class="form-control" placeholder="ex: Fulano da Silva" />
+      <input type="text" id="name" name="name" class="form-control" placeholder="ex: Fulano da Silva" required />
       <label class="form-label" for="name">Nome completo</label>
     </div>
 
     <!-- Email input -->
     <div class="form-outline mb-4">
-      <input type="email" id="email" name="email" class="form-control" placeholder="ex: fulano@email.com.br" />
+      <input type="email" id="email" name="email" class="form-control" placeholder="ex: fulano@email.com.br" required />
       <label class="form-label" for="email">Endereço de E-mail</label>
     </div>
 
     <!-- Phone input -->
     <div class="form-outline mb-4">
-      <input type="tel" id="phone" name="phone" class="form-control" placeholder="ex: 21 6546-5555" />
+      <input type="tel" id="phone" name="phone" class="form-control" placeholder="ex: 21-65465555" pattern="[0-9]{2}-[0-9]{8}" required />
       <label class="form-label" for="phone">Telefone com DDD</label>
     </div>
 
