5c7f0f6c945bd1b026de7539836efd0e3b22e98d
israelcena <israelsjm@gmail.com>
2022-06-11 17:49:40 -0300

    fix active class

--- public/components/nav.php
+++ public/components/nav.php
@@ -7,13 +7,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav m-auto mb-2 mb-lg-0 fs-5">
         <li class="nav-item me-3">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
+          <a class="nav-link <?php echo $_SERVER['REQUEST_URI'] == "/" ?  "active" : ""; ?>" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item me-3">
-          <a class="nav-link" href="/listar">Listar</a>
+          <a class="nav-link <?php echo $_SERVER['REQUEST_URI'] == "/listar" ? "active" : ""; ?>" href="/listar">Listar</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/cadastrar">Cadastrar</a>
+          <a class="nav-link <?php echo $_SERVER['REQUEST_URI'] == "/cadastrar" ? "active" : ""; ?>" href="/cadastrar">Cadastrar</a>
         </li>
       </ul>
       <form class="d-flex" action="/buscar">
