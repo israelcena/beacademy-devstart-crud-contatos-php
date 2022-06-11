183ab64e1203d9ed48aacc9c70a0c8d16bc3a322
israelcena <israelsjm@gmail.com>
2022-06-11 17:44:10 -0300

    list all contacts

--- public/components/list.php
+++ public/components/list.php
@@ -1,3 +1,27 @@
-<?php
-
-echo "Listando...";
+<div class="container mt-5">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th scope="col">Nome</th>
+        <th scope="col">E-mail</th>
+        <th scope="col">Telefone</th>
+        <th scope="col">Ações</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      foreach ($contacts as $i => $contact) {
+        $contactInfos = explode(",", $contact);
+        echo "<tr>";
+        echo '<th scope="row">' . $i + 1 . '</th>';
+        echo "<td>{$contactInfos[0]}</td>";
+        echo "<td>{$contactInfos[1]}</td>";
+        echo "<td>{$contactInfos[2]}</td>";
+        echo '<td class="d-block me-auto"><button class="btn btn-primary btn-sm me-2">Editar</button><button class="btn btn-danger btn-sm">Excluir</button></td>';
+        echo "</tr>";
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
