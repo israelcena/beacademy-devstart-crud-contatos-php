4c77f8fce59e9974e0600dbfa2c9459da7ef8123
israelcena <israelsjm@gmail.com>
2022-06-11 15:18:01 -0300

    rework main

--- public/components/main.php
+++ public/components/main.php
@@ -1,19 +1,11 @@
-<div class="container">
+<div class="container-fluid ">
   <div class="row">
-    <div class="col-xs-4">
-      <div class="card">
-        <div class="card-body">
-          <h3 class="card-title">Title</h3>
-          <p class="card-text">Text</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-xs-4">
-      <div class="card">
-        <div class="card-body">
-          <h3 class="card-title">Title</h3>
-          <p class="card-text">Text</p>
-        </div>
+    <div class="card text-center bg-dark text-white ">
+      <img src="https://images.unsplash.com/photo-1520923642038-b4259acecbd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2019&q=80" class="card-img" alt="Contatos">
+      <div class="card-img-overlay mt-5">
+        <h5 class="card-title fs-1">Bem-Vindo!</h5>
+        <p class="card-text fs-4">Esse programa foi desenvolvido para armazenar e gerenciar contatos! </br> Todos eles ficam armazenados em um arquivo em .csv e você poderá salvar-lós rapidamente.</p>
+        <p class="card-text fs-2">Feito por Israel Cena</p>
       </div>
     </div>
   </div>
